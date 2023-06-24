54bc7d0d39d19461ccc20136ed78a8cfbb42f349
Grazziano Fagundes <grazzianofagundes@gmail.com>
2023-06-24 17:11:54 -0300

    Trocando tipos de login

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -8,6 +8,8 @@ import {
 } from 'react-native';
 
 export default function Login() {
+  const [type, setType] = useState('login');
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -31,12 +33,26 @@ export default function Login() {
         onChangeText={(text) => setPassword(text)}
       />
 
-      <TouchableOpacity style={styles.handleLogin} onPress={handleLogin}>
-        <Text style={styles.loginText}>Acessar</Text>
+      <TouchableOpacity
+        style={[
+          styles.handleLogin,
+          { backgroundColor: type === 'login' ? '#3EA6F2' : '#141414' },
+        ]}
+        onPress={handleLogin}
+      >
+        <Text style={styles.loginText}>
+          {type === 'login' ? 'Acessar' : 'Cadastrar'}
+        </Text>
       </TouchableOpacity>
 
-      <TouchableOpacity>
-        <Text style={{ textAlign: 'center' }}>Criar uma conta</Text>
+      <TouchableOpacity
+        onPress={() =>
+          setType((type) => (type === 'login' ? 'cadastrar' : 'login'))
+        }
+      >
+        <Text style={{ textAlign: 'center' }}>
+          {type === 'login' ? 'Criar uma conta' : 'Já possuo uma conta'}
+        </Text>
       </TouchableOpacity>
     </SafeAreaView>
   );
@@ -61,7 +77,6 @@ const styles = StyleSheet.create({
   handleLogin: {
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#141414',
     height: 45,
     marginBottom: 10,
   },

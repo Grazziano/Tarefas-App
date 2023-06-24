985c30411e92c1b2dce5fb70411418d1856e893c
Grazziano Fagundes <grazzianofagundes@gmail.com>
2023-06-24 17:03:01 -0300

    Iniciando projeto

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import Login from './src/components/Login';
 
 export default function App() {
+  const [user, setUser] = useState(null);
+
+  if (!user) {
+    return <Login />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Dentro da tela tarefas</Text>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 25,
+    paddingHorizontal: 10,
   },
 });
